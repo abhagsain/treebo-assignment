5e1945e725cca3e62409314dedcca271ede66306
Anurag Bhagsain <abhagsain@gmail.com>
2020-12-04 10:31:12 +0530

    feat: ✅ add variants for heading

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,6 +1,13 @@
 import React from "react";
-import "./heading.css";
 import cx from "classnames";
-export default function Heading({ className, children }) {
-  return <h2 className={cx("heading", className)}>{children}</h2>;
+export default function Heading({ className, variant, children }) {
+  const fonts = {
+    xs: "text-xs",
+    sm: "text-sm",
+    base: "text-base",
+    lg: "text-lg",
+    xl: "text-xl",
+    "2xl": "text-2xl",
+  };
+  return <h2 className={cx(className, fonts[variant])}>{children}</h2>;
 }
